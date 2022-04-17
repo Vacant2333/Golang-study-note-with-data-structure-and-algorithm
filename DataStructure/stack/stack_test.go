8fc1365289f0fc7e1d6cb9168712589d42543d8e
Vacant2333 <vacant2333@gmail.com>
2022-04-17 21:35:24 +0800

    queue,stack 测试

---
+++ DataStructure/stack/stack_test.go
@@ -0,0 +1,29 @@
+package stack
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCreate(t *testing.T) {
+	head := Create()
+	fmt.Println(head)
+}
+
+func TestNode_Push(t *testing.T) {
+	head := Create()
+	head.Push(1)
+	head.Push(2)
+	head.Push(3)
+	fmt.Println(head.Next)
+}
+
+func TestNode_Pop(t *testing.T) {
+	head := Create()
+	head.Push(1)
+	head.Push(2)
+	head.Push(3)
+	fmt.Println(head.Pop(true))
+	fmt.Println(head.Pop(true))
+	fmt.Println(head.Pop(true))
+}
