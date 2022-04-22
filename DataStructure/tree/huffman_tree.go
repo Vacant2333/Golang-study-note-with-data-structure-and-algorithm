4f86936a835ab9a09050dc5c08417bf6f8831248
Vacant2333 <vacant2333@gmail.com>
2022-04-22 11:13:34 +0800

    更新

--- DataStructure/tree/huffman_tree.go
+++ DataStructure/tree/huffman_tree.go
@@ -1,5 +1,10 @@
 package tree
 
+import (
+	"errors"
+	"fmt"
+)
+
 /*
 	哈夫曼树主要用于对文本进行编码,压缩文本大小
 	哈夫曼树的原则:
@@ -8,5 +13,24 @@ package tree
 		3.只有叶节点用来存数据
 		4.没有度为1的节点
 		5.n个叶子结点的哈夫曼树共有2n-1个节点
-	哈夫曼树用于编码的时候左子树是0右子树是1
+	哈夫曼树用于编码的时候左子树代表0右子树代表1,从根节点走到某叶节点时的路径(0/1)就是该叶节点的编码
 */
+
+// BuildHuffmanTree 从一个slice生成哈夫曼树(数据应按频率的升序存放,频率低的数据放在前面)
+func BuildHuffmanTree(s []ElementType) Node {
+	if len(s) <= 1 {
+		panic(errors.New("slice need more element"))
+	}
+	n := buildConnectNode(nil, nil)
+
+	fmt.Println(n.Data == "")
+	return n
+}
+
+// buildConnectNode 创建一个连接节点(data为空)
+func buildConnectNode(left, right *Node) Node {
+	n := Node{}
+	n.Left = left
+	n.Right = right
+	return n
+}
---
+++ DataStructure/tree/huffman_tree_test.go
@@ -0,0 +1,7 @@
+package tree
+
+import "testing"
+
+func TestBuildHuffmanTree(t *testing.T) {
+	BuildHuffmanTree([]ElementType{"1", "2"})
+}
