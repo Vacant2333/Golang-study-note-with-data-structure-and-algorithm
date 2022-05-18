fa4c515bb13cea7db7aa55c43572d26878dfb7e1
Vacant2333 <vacant2333@gmail.com>
2022-05-18 21:22:56 +0800

    http

--- Project/http_server_demo/main.go
+++ Project/http_server_demo/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
 
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/counter", counter)
+	http.ListenAndServe("localhost:8000", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+
+	fmt.Fprintf(w, "URL.Path = %v %v", r.URL.Path, r.URL.User)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "count: %d", count)
+	mu.Unlock()
 }
