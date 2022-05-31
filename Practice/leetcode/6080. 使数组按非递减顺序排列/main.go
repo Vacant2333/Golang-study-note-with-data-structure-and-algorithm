45de0e59ba33bf235aa8b200198c554157015f06
Vacant2333 <vacant2333@gmail.com>
2022-05-31 10:21:43 +0800

    没做出来

--- Practice/leetcode/6080. 使数组按非递减顺序排列/main.go
+++ Practice/leetcode/6080. 使数组按非递减顺序排列/main.go
@@ -5,28 +5,7 @@ import "fmt"
 // 没做出来
 
 func totalSteps(nums []int) int {
-	max := 0
-	for i := 0; i < len(nums); i++ {
-		tmp := 0
-		lastIndex := i + 1
-		for x := i + 1; x < len(nums); x++ {
-			if nums[x] < nums[lastIndex] {
-				break
-			} else {
-				lastIndex = x
-			}
-
-			if nums[x] < nums[i] {
-				tmp++
-			} else {
-				break
-			}
-		}
-		if tmp > max {
-			max = tmp
-		}
-	}
-	return max
+	return 0
 }
 
 func main() {
