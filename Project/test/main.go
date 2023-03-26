927cf2392035593cd17945610fe6e08fe417746c
Vacant2333 <vacant2333@gmail.com>
2023-03-26 22:48:18 +0800

    update

--- Project/test/main.go
+++ Project/test/main.go
@@ -3,10 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	a1 := []int{12, 1, 2, 3, 4, 5, 6, 14, 13}
-
-	for _, v := range a1 {
-		a1 = []int{1}
-		fmt.Println(v)
+	count := 0
+	fmt.Scanf("%d", &count)
+	for i := 0; i < count; i++ {
+		// 车数量
+		carCount := 0
+		fmt.Scanf("%d", &carCount)
+		stack := make([]int, 0)
+		push := make([]int, carCount)
+		pop := make([]int, carCount)
+		for i := 0; i < carCount; i++ {
+			fmt.Scanf("%d", &push[i])
+		}
+		for i := 0; i < carCount; i++ {
+			fmt.Scanf("%d", &pop[i])
+		}
+		c := 0
+		for _, x := range push {
+			stack = append(stack, x)
+			for len(stack) > 0 && stack[len(stack)-1] == pop[c] {
+				stack = stack[:len(stack)-1]
+				c++
+			}
+		}
+		if len(stack) == 0 {
+			fmt.Println("Yes")
+		} else {
+			fmt.Println("No")
+		}
 	}
 }
