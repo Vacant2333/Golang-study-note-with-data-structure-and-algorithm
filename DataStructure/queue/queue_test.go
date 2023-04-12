595080773d520974e467147f61aa42dab2d6879f
Vacant2333 <vacant2333@gmail.com>
2023-04-12 15:20:20 +0800

    add unit test

--- DataStructure/queue/queue_test.go
+++ DataStructure/queue/queue_test.go
@@ -1,29 +1,118 @@
 package queue
 
 import (
-	"fmt"
+	"errors"
 	"testing"
 )
 
-func TestCreate(t *testing.T) {
-	head := Create()
-	fmt.Println(head)
-}
+func TestQueueOperations(t *testing.T) {
+	testCases := []struct {
+		name          string
+		operations    []string
+		values        []int
+		expectedData  []int
+		expectedError error
+	}{
+		{
+			name:          "Create and push elements",
+			operations:    []string{"push", "push", "push"},
+			values:        []int{1, 2, 3},
+			expectedData:  []int{1, 2, 3},
+			expectedError: nil,
+		},
+		{
+			name:          "Pop without deleting",
+			operations:    []string{"push", "push", "pop"},
+			values:        []int{1, 2, 0},
+			expectedData:  []int{1},
+			expectedError: nil,
+		},
+		{
+			name:          "Pop with deleting",
+			operations:    []string{"push", "push", "pop"},
+			values:        []int{1, 2, 1},
+			expectedData:  []int{1},
+			expectedError: nil,
+		},
+		{
+			name:          "Pop empty queue",
+			operations:    []string{"pop"},
+			values:        []int{1},
+			expectedData:  []int{},
+			expectedError: errors.New("queue is empty"),
+		},
+		{
+			name:          "Create queue and check if empty",
+			operations:    []string{"isEmpty"},
+			values:        []int{0},
+			expectedData:  []int{1},
+			expectedError: nil,
+		},
+		{
+			name:          "Push element to queue and check if empty",
+			operations:    []string{"push", "isEmpty"},
+			values:        []int{1, 0},
+			expectedData:  []int{0},
+			expectedError: nil,
+		},
+		{
+			name:          "Push multiple elements and pop one by one",
+			operations:    []string{"push", "push", "push", "pop", "pop", "pop"},
+			values:        []int{1, 2, 3, 1, 1, 1},
+			expectedData:  []int{1, 2, 3},
+			expectedError: nil,
+		},
+		{
+			name:          "Attempt to pop from empty queue",
+			operations:    []string{"pop"},
+			values:        []int{1},
+			expectedData:  []int{},
+			expectedError: errors.New("queue is empty"),
+		},
+		{
+			name:          "Pop and push elements consecutively",
+			operations:    []string{"push", "push", "pop", "push", "pop", "push"},
+			values:        []int{1, 2, 1, 3, 1, 4},
+			expectedData:  []int{1, 2},
+			expectedError: nil,
+		},
+	}
 
-func TestNode_Push(t *testing.T) {
-	head := Create()
-	head.Push(1)
-	head.Push(2)
-	head.Push(3)
-	fmt.Println(head.Next)
-}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			q := Create()
+			expectedDataIdx := 0
 
-func TestNode_Pop(t *testing.T) {
-	head := Create()
-	head.Push(1)
-	head.Push(2)
-	head.Push(3)
-	fmt.Println(head.Pop(true))
-	fmt.Println(head.Pop(true))
-	fmt.Println(head.Pop(true))
+			for i, op := range tc.operations {
+				switch op {
+				case "push":
+					q.Push(tc.values[i])
+				case "pop":
+					delete := tc.values[i] == 1
+					val, err := q.Pop(delete)
+					if err != nil && tc.expectedError == nil {
+						t.Errorf("Expected no error, got %v", err)
+					} else if err == nil && tc.expectedError != nil {
+						t.Errorf("Expected error %v, got no error", tc.expectedError)
+					} else if err != nil && tc.expectedError != nil && err.Error() != tc.expectedError.Error() {
+						t.Errorf("Expected error %v, got %v", tc.expectedError, err)
+					}
+
+					if delete && expectedDataIdx < len(tc.expectedData) && val != tc.expectedData[expectedDataIdx] {
+						t.Errorf("Expected value %d, got %d", tc.expectedData[expectedDataIdx], val)
+					}
+					if delete {
+						expectedDataIdx++
+					}
+				case "isEmpty":
+					isEmpty := q.isEmpty()
+					expectedIsEmpty := tc.expectedData[expectedDataIdx] == 1
+					expectedDataIdx++
+					if isEmpty != expectedIsEmpty {
+						t.Errorf("Expected isEmpty to be %t, got %t", expectedIsEmpty, isEmpty)
+					}
+				}
+			}
+		})
+	}
 }
--- Project/test/main.go
+++ Project/test/main.go
@@ -1,21 +1,5 @@
 package main
 
-import "sort"
-
 func main() {
-}
 
-func eraseOverlapIntervals(intervals [][]int) int {
-	L := len(intervals)
-	sort.Slice(intervals, func(i, j int) bool {
-		return intervals[i][1] < intervals[j][1]
-	})
-	ans, right := 1, intervals[0][1]
-	for i := 0; i < L; i++ {
-		if right <= intervals[i][0] {
-			ans++
-			right = intervals[i][0]
-		}
-	}
-	return L - ans
 }
